4110344e0582e4bde920d42bc1cbf1dc18119fdd
Andrew Stribblehill <andrew.stribblehill@finn.no>
2020-02-07 12:33:24 +0100

    Fix dumps().
    Also add unit tests for dumping and clean up (parameterise) the tests.

--- jsondatetime/jsondatetime.py
+++ jsondatetime/jsondatetime.py
@@ -1,5 +1,7 @@
-import json
 import datetime
+import functools
+import json
+
 import dateutil.parser
 
 try:
@@ -7,36 +9,23 @@ try:
 except NameError:
     string_types = str  # Python 3
 
-DEFAULT_DATE_FORMAT = '%a, %d %b %Y %H:%M:%S UTC'
-DEFAULT_ARGUMENT = "datetime_format"
 
 class DatetimeJSONEncoder(json.JSONEncoder):
-
     def default(self, obj):
-        if isinstance(obj, datetime.datetime) or isinstance(obj, datetime.date):
+        if isinstance(obj, (datetime.date, datetime.datetime)):
             return obj.isoformat()
-        else:
-            return json.JSONEncoder.default(obj)
-
-json._default_encoder = DatetimeJSONEncoder
+        super(DatetimeJSONEncoder, self).default(obj)
 
 
-def dumps(obj, skipkeys=False, ensure_ascii=True, check_circular=True,
-          allow_nan=True, cls=None, indent=None, separators=None,
-          encoding='utf-8', default=None, sort_keys=False, **kw):
-    return json.dumps(obj, skipkeys=skipkeys, ensure_ascii=ensure_ascii,
-                      check_circular=check_circular, allow_nan=allow_nan,
-                      cls=cls, indent=indent, separators=None, encoding=encoding,
-                      default=default, sort_keys=sort_keys, **kw)
+dumps = functools.partial(json.dumps, cls=DatetimeJSONEncoder)
+
 
 def loads(s, **kwargs):
-
     source = json.loads(s, **kwargs)
-
     return iteritems(source)
 
-def iteritems(source):
 
+def iteritems(source):
     for k, v in source.items():
         if isinstance(v, list):
             for a in v:
@@ -45,7 +34,7 @@ def iteritems(source):
             iteritems(v)
         elif isinstance(v, string_types):
             try:
-                source[k] = dateutil.parser.parse(v, ignoretz=True)
+                source[k] = dateutil.parser.parse(v)
             except:
                 pass
 
