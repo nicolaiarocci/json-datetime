a16ced3389bfe551dee43eb87a687c92a98b8ceb
Andrew Stribblehill <stribb@gmail.com>
2020-02-15 17:58:45 +0100

    Only use Black for Python 3 (and above).

--- script/test.sh
+++ script/test.sh
@@ -2,6 +2,10 @@
 set -euvo pipefail
 IFS=$'\n\t'
 
+has() {
+    type "$1" >/dev/null 2>&1
+}
+
 cd jsondatetime
 
 # stop the build if there are Python syntax errors or undefined names
@@ -9,6 +13,6 @@ flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
 # exit-zero treats all errors as warnings. The GitHub editor is 127 chars wide
 flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
 # Ensure style conformity
-black --check .
+has black && black --check .
 # Make sure it passes its tests
 pytest
