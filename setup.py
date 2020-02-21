6926274e9dfd6990b98b8aa9276f5515b1879e9a
Andrew Stribblehill <stribb@gmail.com>
2020-02-21 15:18:37 +0100

    Fix `setup.py`.
    `license` should be one-line otherwise it breaks `PKG-INFO` parsing.
    
    Change README format to Markdown.

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
 
+from os import path
+
 from setuptools import find_packages, setup
 
-DESCRIPTION = (
+description = (
     "Allows for encoding and decoding of datetime values contained in JSON streams"
 )
-LONG_DESCRIPTION = open("README.rst").read()
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="jsondatetime",
-    version="0.1.0",
-    description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
+    version="0.1.1",
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     # Forked.
     #  Original author='Nicola Iarocci',
     #  Original author_email='nicola@nicolaiarocci.com',
@@ -19,7 +25,7 @@ setup(
     author="Andrew Stribblehill",
     author_email="andrew.stribblehill@schibsted.com",
     url="https://github.com/schibsted/json-datetime",
-    license=open("LICENSE").read(),
+    license="ISCL",
     platforms=["any"],
     packages=find_packages(),
     test_suite="jsondatetime.tests",
